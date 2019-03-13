5d26d832558ab9e0c916892354fc20fd07b74392
Patrick Venture <venture@google.com>
2019-03-13 16:31:09 -0700

    eth: move structure to header
    Move the structure definition to the header file to expose it to the
    testing code.
    
    Change-Id: I019a322364e5d6c5cc9d79419c854ee47b65e03e
    Signed-off-by: Patrick Venture <venture@google.com>

--- eth.hpp
+++ eth.hpp
@@ -7,6 +7,18 @@ namespace google
 namespace ipmi
 {
 
+// The reply to the ethdevice command specifies the
+// IPMI channel number and the if_name used for the
+// ncis connection.
+struct EthDeviceReply
+{
+    uint8_t subcommand;
+    uint8_t channel;
+    // if_name_len doesn't include the null-terminator.
+    uint8_t if_name_len;
+    uint8_t if_name[0];
+} __attribute__((packed));
+
 // Handle the eth query command.
 // Sys can query the if_name and IPMI channel of the BMC's NCSI ethernet
 // device.
