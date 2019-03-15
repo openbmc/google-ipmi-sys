c1cb60eb6dcf91411ec2ae73c3d53d51159fb5b3
Patrick Venture <venture@google.com>
2019-03-15 13:42:06 -0700

    add handler logic to handle SysCpldVersion
    Add handler logic to handler for SysCpldVersion such that it splits the
    true IPMI processing from the business logic.
    
    Tested: Only ran unit-tests (added new ones).
    Change-Id: I09d95d8be8fbe75648b3332af898336b00074c2f
    Signed-off-by: Patrick Venture <venture@google.com>

--- cpld.hpp
+++ cpld.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "handler.hpp"
+
 #include <ipmid/api.h>
 
 namespace google
@@ -9,7 +11,8 @@ namespace ipmi
 
 // Given a cpld identifier, return a version if available.
 ipmi_ret_t CpldVersion(const uint8_t* reqBuf, uint8_t* replyBuf,
-                       size_t* dataLen);
+                       size_t* dataLen,
+                       const HandlerInterface* handler = &handlerImpl);
 
 } // namespace ipmi
 } // namespace google
