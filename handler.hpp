bd4041e492d8ac373f1ce10fb3fe1221e370caee
William A. Kennington III <wak@google.com>
2020-05-18 15:06:07 -0700

    handler: Add more entity IDs
    Change-Id: I77b8a78deda33d9f15b6c69a1f6950f1dacc5170
    Signed-off-by: William A. Kennington III <wak@google.com>

--- handler.hpp
+++ handler.hpp
@@ -122,8 +122,10 @@ class Handler : public HandlerInterface
         {0x0B, "add_in_card"},
         {0x0E, "power_system_board"},
         {0x10, "system_internal_expansion_board"},
+        {0x11, "other_system_board"},
         {0x17, "system_chassis"},
         {0x1D, "fan"},
+        {0x1E, "cooling_unit"},
         {0x20, "memory_device"}};
 
     nlohmann::json _entityConfig{};
