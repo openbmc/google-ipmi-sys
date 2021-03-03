2cca4f2d13cdd98bd7cda5097c633d088ca9e2e6
Willy Tu <wltu@google.com>
2021-03-02 17:58:10 -0800

    Add OEM command to request flash size
    The BMC flash is `/dev/mtd0` anad the size can found in
    `/sys/class/mtd/mtd0/size`. The OEM command will request that
    information with MTDINFO.
    
    Tested:
    Successfully fetch the flash size information with ipmitool.
    
    ```
    cat /sys/class/mtd/mtd0/size
    67108864
    ```
    67108864 / 1024 / 1024 = 64MB flash
    
    ```
    ipmitool raw 0x2e 0x32 0x79 0x2b 0x00 0x09
     79 2b 00 09 00 00 00 04
    ```
    Output in little endian.
    0x04000000 -> 67108864
    
    Change-Id: Iec1b33503d1166a42ceef4b8491e5c19c3a077fe
    Signed-off-by: Willy Tu <wltu@google.com>

---
+++ flash_size.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "handler.hpp"
+
+#include <ipmid/api.h>
+
+namespace google
+{
+namespace ipmi
+{
+
+ipmi_ret_t getFlashSize(const uint8_t* reqBuf, uint8_t* replyBuf,
+                        size_t* dataLen, HandlerInterface* handler);
+
+} // namespace ipmi
+} // namespace google
