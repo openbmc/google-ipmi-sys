f751aa34870d768cf1c6a5f2092fb3cec7f664b7
Steve Foreman <foremans@google.com>
2021-09-20 14:06:32 -0700

    Add IPMI interface for CustomAccel service
    Change-Id: I28a8976e382b457233ac521e9ab71f75abe029d1
    Signed-off-by: Steve Foreman <foremans@google.com>

---
+++ google_accel_oob.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "handler.hpp"
+
+#include <ipmid/api.h>
+
+namespace google
+{
+namespace ipmi
+{
+
+//  Handle the Accel OOB device count command
+ipmi_ret_t accelOobDeviceCount(const uint8_t* reqBuf, uint8_t* replyBuf,
+                               size_t* dataLen, HandlerInterface* handler);
+
+ipmi_ret_t accelOobDeviceName(const uint8_t* reqBuf, uint8_t* replyBuf,
+                              size_t* dataLen, HandlerInterface* handler);
+
+ipmi_ret_t accelOobRead(const uint8_t* reqBuf, uint8_t* replyBuf,
+                        size_t* dataLen, HandlerInterface* handler);
+
+ipmi_ret_t accelOobWrite(const uint8_t* reqBuf, uint8_t* replyBuf,
+                        size_t* dataLen, HandlerInterface* handler);
+
+} // namespace ipmi
+} // namespace google
