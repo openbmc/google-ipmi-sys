3c0e209e50022b69b7e7798c9d5fc9944b738fdc
Willy Tu <wltu@google.com>
2021-10-10 13:34:41 -0700

    pcie_bifurcation: Add IPMI command to fetch the highest level bifurcation at a PCIe slot
    Use the new FruDevice Decreator `Bifurcation` to fetch the bifurcation at each device.
    
    Tested:
    Unit tests passed.
    
    Physical Tests.
    
    PE0 -> no bifurcation
    PE1 -> x8x8
    
    ```
    $ ipmitool raw 0x2e 0x32 0x79 0x2b 0x00 0x0b 0
     79 2b 00 0b 00
    
    $ ipmitool raw 0x2e 0x32 0x79 0x2b 0x00 0x0b 1
     79 2b 00 0b 02 08 08
    ```
    
    Change-Id: Id811897cad82a36da29cd8795f8bc6e855a7e96d
    Signed-off-by: Willy Tu <wltu@google.com>

--- test/handler_mock.hpp
+++ test/handler_mock.hpp
@@ -53,6 +53,8 @@ class HandlerMock : public HandlerInterface
     MOCK_METHOD((std::tuple<std::uint32_t, std::string>), getI2cEntry,
                 (unsigned int), (const, override));
     MOCK_METHOD(void, hostPowerOffDelay, (std::uint32_t), (const, override));
+    MOCK_METHOD(std::vector<uint8_t>, pcieBifurcation,
+                (std::uint8_t, const std::string&), (override));
 };
 
 } // namespace ipmi
