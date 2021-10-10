1930ffffdc9a8af04fcd38a458e05714417a1bfa
Willy Tu <wltu@google.com>
2021-10-10 13:34:41 -0700

    pcie_bifurcation: Add IPMI command to fetch the highest level bifurcation at a PCIe slot
    Use the new FruDevice Decreator `Bifurcation` to fetch the bifurcation at each device.
    
    Tested:
    Unit tests passed.
    
    Physical Tests.
    
    PE0 -> no bifurcation
    PE1 -> x8x8
    
    ```
    $ ipmitool raw 0x2e 0x32 0x79 0x2b 0x00 0x0b 0
     79 2b 00 0b 00
    
    $ ipmitool raw 0x2e 0x32 0x79 0x2b 0x00 0x0b 1
     79 2b 00 0b 02 08 08
    ```
    
    Change-Id: Id811897cad82a36da29cd8795f8bc6e855a7e96d
    Signed-off-by: Willy Tu <wltu@google.com>

---
+++ bifurcation/bifurcation.cpp
@@ -0,0 +1,197 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include "bifurcation.hpp"
+
+#include <fmt/format.h>
+
+#include <charconv>
+#include <filesystem>
+#include <memory>
+#include <optional>
+#include <sdbusplus/bus.hpp>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+namespace google
+{
+namespace ipmi
+{
+
+using Value = std::variant<uint64_t, std::vector<uint64_t>>;
+
+using ObjectType =
+    std::unordered_map<std::string, std::unordered_map<std::string, Value>>;
+using ManagedObjectType =
+    std::unordered_map<sdbusplus::message::object_path, ObjectType>;
+
+constexpr static const char* kObjectManagerServiceName =
+    "org.freedesktop.DBus.ObjectManager";
+constexpr static const char* kEntityManagerServiceName =
+    "xyz.openbmc_project.EntityManager";
+constexpr static const char* kFruDeviceInterfaceName =
+    "xyz.openbmc_project.Inventory.Decorator.FruDevice";
+
+std::optional<std::vector<uint8_t>> Bifurcation::getBifurcation(uint8_t bus)
+{
+    ManagedObjectType entities;
+    auto systemBus = sdbusplus::bus::new_default();
+    auto getObjects = systemBus.new_method_call(kEntityManagerServiceName, "/",
+                                                kObjectManagerServiceName,
+                                                "GetManagedObjects");
+    auto resp = systemBus.call(getObjects);
+    resp.read(entities);
+    for (const auto& entry : entities)
+    {
+        auto findFruDevice = entry.second.find(kFruDeviceInterfaceName);
+        if (findFruDevice == entry.second.end())
+        {
+            continue;
+        }
+
+        auto findBus = findFruDevice->second.find("Bus");
+        auto findAddress = findFruDevice->second.find("Address");
+        auto findBifurcation = findFruDevice->second.find("Bifurcation");
+        if (findBus == findFruDevice->second.end() ||
+            findAddress == findFruDevice->second.end() ||
+            findBifurcation == findFruDevice->second.end())
+        {
+            continue;
+        }
+
+        if (!std::holds_alternative<uint64_t>(findBus->second) ||
+            std::get<uint64_t>(findBus->second) != bus)
+        {
+            continue;
+        }
+
+        if (!std::holds_alternative<std::vector<uint64_t>>(
+                findBifurcation->second))
+        {
+            continue;
+        }
+
+        auto bifurcation =
+            std::get<std::vector<uint64_t>>(findBifurcation->second);
+        return std::vector<uint8_t>(bifurcation.begin(), bifurcation.end());
+    }
+
+    return std::nullopt;
+}
+
+std::vector<uint8_t> Bifurcation::walkChannel(std::string_view basePath,
+                                              std::string_view path,
+                                              Cache& cache)
+{
+    // Use only the first device
+    for (const auto& entry : std::filesystem::directory_iterator(path))
+    {
+        const std::string& device = entry.path().filename();
+
+        uint8_t bus = 0;
+        // i2c-63
+        //     ^ (start looking for bus number there)
+        auto [ptr, ec] = std::from_chars(device.data() + 4,
+                                         device.data() + device.size(), bus);
+        if (ec != std::errc() || ptr != device.data() + device.size())
+        {
+            continue;
+        }
+
+        return walkI2CTreeBifurcation(basePath, bus, cache);
+    }
+    return {};
+}
+
+std::vector<std::vector<uint8_t>>
+    Bifurcation::walkMux(std::string_view basePath, std::string_view path,
+                         Cache& cache)
+{
+    std::vector<std::vector<uint8_t>> output;
+    std::vector<std::string> channels;
+
+    for (const auto& entry : std::filesystem::directory_iterator(path))
+    {
+        const std::string& channelPath = entry.path().filename();
+        if (channelPath.starts_with("channel-"))
+        {
+            channels.push_back(fmt::format("{}/i2c-dev", entry.path().c_str()));
+        }
+    }
+
+    // Make sure to read from channel-0 to channel-N.
+    std::sort(channels.begin(), channels.end());
+    for (const auto& channel : channels)
+    {
+        output.push_back(walkChannel(basePath, channel, cache));
+    }
+
+    return output;
+}
+
+// Assuming that there are no loops.
+// It will prevent loops, but the result wil be wrong if loops exists.
+// If there are loops, the kernel is setup wrong.
+std::vector<uint8_t>
+    Bifurcation::walkI2CTreeBifurcation(std::string_view basePath, uint8_t bus,
+                                        Cache& cache)
+{
+    if (cache.count(bus) > 0)
+    {
+        return {};
+    }
+
+    auto maybeBifurcation = getBifurcation(bus);
+    if (!maybeBifurcation)
+    {
+        return {};
+    }
+
+    cache.insert(bus);
+    std::vector<std::vector<uint8_t>> output;
+
+    auto path = fmt::format("{}/i2c-{}", basePath, bus);
+    for (const auto& entry : std::filesystem::directory_iterator(path))
+    {
+        const std::string& i2cPath = entry.path().filename();
+
+        if (i2cPath.starts_with(fmt::format("{}-", bus)))
+        {
+            auto muxPath = fmt::format("{}/{}", path, i2cPath);
+            output = walkMux(basePath, muxPath, cache);
+            break;
+        }
+    }
+
+    std::vector<uint8_t> result;
+    for (size_t i = 0; i < maybeBifurcation->size(); ++i)
+    {
+        if (output[i].empty())
+        {
+            result.emplace_back((*maybeBifurcation)[i]);
+        }
+        else
+        {
+            result.insert(result.end(), output[i].begin(), output[i].end());
+        }
+    }
+
+    return result;
+}
+
+} // namespace ipmi
+} // namespace google
