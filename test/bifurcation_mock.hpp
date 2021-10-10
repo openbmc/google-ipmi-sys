45692d5f590c4676458719b2cba066d93ac68d96
Willy Tu <wltu@google.com>
2021-10-10 13:34:41 -0700

    pcie_bifurcation init
    Change-Id: Id811897cad82a36da29cd8795f8bc6e855a7e96d
    Signed-off-by: Willy Tu <wltu@google.com>

--- test/bifurcation_mock.hpp
+++ test/bifurcation_mock.hpp
@@ -14,24 +14,27 @@
 
 #pragma once
 
-#include "handler_mock.hpp"
+#include "bifurcation.hpp"
 
-#include <ipmid/api-types.hpp>
-#include <utility>
-#include <vector>
+#include <string>
+#include <tuple>
 
-#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
 namespace google
 {
 namespace ipmi
 {
 
-// Validate the return code and the data for the IPMI reply.
-// Returns the subcommand and the optional informations.
-std::pair<std::uint8_t, std::vector<std::uint8_t>> ValidateReply(
-    ::ipmi::RspType<std::uint8_t, std::optional<std::vector<uint8_t>>> reply,
-    bool hasData = true);
+class BifurcationMock : public BifurcationInterface
+{
+
+  public:
+    ~BifurcationMock() = default;
+
+    MOCK_METHOD(std::optional<std::vector<uint8_t>>, getBifurcation,
+                (std::uint8_t), (override));
+};
 
 } // namespace ipmi
 } // namespace google
