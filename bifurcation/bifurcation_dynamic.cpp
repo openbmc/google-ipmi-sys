e7572330500e3c796d084e9e3551b07f36b909a3
Willy Tu <wltu@google.com>
2021-10-10 13:34:41 -0700

    pcie_bifurcation: Fetch highest level bifurcation at a PCIe slot
    Use the MaxLanes property defined in PCIeDevice and Lanes property
    defined in PCIeSlot to construct the bifurication info.
    
    The command will parse the i2c tree until it hit the PCIe device and
    return the lanes that it uses. At each PCIe slots, it will try to find
    the devices connected to it. All of the PCIe Devices/Slots needs to be
    avaliable in FruDevice for this to work.
    
    Tested:
    Unit tests passed.
    
    Physical Tests.
    
    PE0 -> no bifurcation
    PE1 -> x8x8
    
    ```
    $ ipmitool raw 0x2e 0x32 0x79 0x2b 0x00 0x0f 0
     79 2b 00 0b 00
    
    $ ipmitool raw 0x2e 0x32 0x79 0x2b 0x00 0x0f 1
     79 2b 00 0b 02 08 08
    ```
    
    Change-Id: Id811897cad82a36da29cd8795f8bc6e855a7e96d
    Signed-off-by: Willy Tu <wltu@google.com>

---
+++ bifurcation/bifurcation_dynamic.cpp
@@ -0,0 +1,309 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include "bifurcation.hpp"
+
+#include <fmt/format.h>
+
+#include <charconv>
+#include <filesystem>
+#include <memory>
+#include <optional>
+#include <sdbusplus/bus.hpp>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <utility>
+#include <variant>
+#include <vector>
+
+namespace google
+{
+namespace ipmi
+{
+
+using Value = std::variant<uint64_t, std::vector<uint64_t>>;
+
+using ObjectType =
+    std::unordered_map<std::string, std::unordered_map<std::string, Value>>;
+using ManagedObjectType =
+    std::vector<std::pair<sdbusplus::message::object_path, ObjectType>>;
+
+constexpr static const char* kObjectManagerServiceName =
+    "org.freedesktop.DBus.ObjectManager";
+constexpr static const char* kEntityManagerServiceName =
+    "xyz.openbmc_project.EntityManager";
+constexpr static const char* kFruDeviceInterfaceName =
+    "xyz.openbmc_project.Inventory.Decorator.I2CDevice";
+constexpr static const char* kPCIeDeviceInterfaceName =
+    "xyz.openbmc_project.Inventory.Item.PCIeDevice";
+constexpr static const char* kPCIeSlotInterfaceName =
+    "xyz.openbmc_project.Inventory.Item.PCIeSlot";
+
+std::optional<bool> BifurcationDynamic::needsUpdate(uint8_t bus)
+{
+    auto currentDevice = pcieResources.find(bus);
+
+    if (currentDevice == pcieResources.end())
+    {
+        return std::nullopt;
+    }
+
+    // Do not override PCIe Device.
+    // PCIe Devices can and will override PCIe slots.
+    // This mean that there are no bifurcation required for the slot.
+    return currentDevice != pcieResources.end() &&
+           currentDevice->second.type == PCIe::Type::Slot;
+}
+
+void BifurcationDynamic::findPCIeDevices()
+{
+    ManagedObjectType entities;
+    try
+    {
+        auto systemBus = sdbusplus::bus::new_default();
+        auto getObjects = systemBus.new_method_call(
+            kEntityManagerServiceName, "/xyz/openbmc_project/inventory",
+            kObjectManagerServiceName, "GetManagedObjects");
+        systemBus.call(getObjects).read(entities);
+    }
+    catch (const sdbusplus::exception::SdBusError&)
+    {
+        return;
+    }
+
+    for (const auto& [_, obj] : entities)
+    {
+        auto findFruDevice = obj.find(kFruDeviceInterfaceName);
+        if (findFruDevice == obj.end())
+        {
+            continue;
+        }
+
+        auto findBus = findFruDevice->second.find("Bus");
+        if (findBus == findFruDevice->second.end())
+        {
+            continue;
+        }
+        uint64_t bus = std::get<uint64_t>(findBus->second);
+        std::optional<bool> shouldUpdate = needsUpdate(bus);
+        if (!shouldUpdate.value_or(true))
+        {
+            continue;
+        }
+        // Has value and we should update. Erase existing resource
+        else if (shouldUpdate.has_value())
+        {
+            pcieResources.erase(bus);
+        }
+
+        PCIe pcie;
+        auto findPCIeDevice = obj.find(kPCIeDeviceInterfaceName);
+        if (findPCIeDevice != obj.end())
+        {
+            auto findMaxLanes = findPCIeDevice->second.find("MaxLanes");
+            if (findMaxLanes != findPCIeDevice->second.end() &&
+                std::holds_alternative<uint64_t>(findMaxLanes->second))
+            {
+                pcie.type = PCIe::Type::Device;
+                pcie.lanes = std::get<uint64_t>(findMaxLanes->second);
+            }
+        }
+
+        auto findPCIeSlot = obj.find(kPCIeSlotInterfaceName);
+        if (findPCIeSlot != obj.end())
+        {
+            auto findLanes = findPCIeSlot->second.find("Lanes");
+            // TODO(wltu): Check to finalize this interface support.
+            auto findChannels = findPCIeSlot->second.find("SupportedChannels");
+            if (findLanes != findPCIeSlot->second.end() &&
+                std::holds_alternative<uint64_t>(findLanes->second) &&
+                findChannels != findPCIeSlot->second.end() &&
+                std::holds_alternative<uint64_t>(findChannels->second))
+            {
+                pcie.type = PCIe::Type::Slot;
+                pcie.lanes = std::get<uint64_t>(findLanes->second);
+                pcie.channels = std::get<uint64_t>(findChannels->second);
+            }
+        }
+
+        if (!std::holds_alternative<uint64_t>(findBus->second))
+        {
+            continue;
+        }
+
+        pcieResources.emplace(bus, pcie);
+    }
+
+    if (!pcieResources.empty())
+    {
+        setup = false;
+    }
+}
+
+std::optional<std::vector<uint8_t>>
+    BifurcationDynamic::getBifurcation(uint8_t index) noexcept
+{
+    Cache cache;
+    if (setup)
+    {
+        findPCIeDevices();
+    }
+
+    return walkI2CTreeBifurcation(i2cPath, index, cache);
+}
+
+std::vector<uint8_t> BifurcationDynamic::walkChannel(std::string_view basePath,
+                                                     std::string_view path,
+                                                     Cache& cache)
+{
+    // Use only the first device
+    for (const auto& entry : std::filesystem::directory_iterator(path))
+    {
+        const std::string& device = entry.path().filename();
+
+        uint8_t bus = 0;
+        // i2c-63
+        //     ^ (start looking for bus number there)
+        auto [ptr, ec] = std::from_chars(device.data() + 4,
+                                         device.data() + device.size(), bus);
+        if (ec != std::errc() || ptr != device.data() + device.size())
+        {
+            continue;
+        }
+
+        return walkI2CTreeBifurcation(basePath, bus, cache);
+    }
+    return {};
+}
+
+std::vector<std::vector<uint8_t>>
+    BifurcationDynamic::walkMux(std::string_view basePath,
+                                std::string_view path, uint8_t channelCount,
+                                Cache& cache)
+{
+    std::vector<std::vector<uint8_t>> output;
+    std::vector<std::string> channels;
+
+    for (const auto& entry : std::filesystem::directory_iterator(path))
+    {
+        const std::string& channelPath = entry.path().filename();
+        if (channelPath.starts_with("channel-"))
+        {
+            channels.push_back(fmt::format("{}/i2c-dev", entry.path().c_str()));
+        }
+    }
+
+    // Make sure to read from channel-0 to channel-N.
+    // This allow the final bifurcation result to be determinsic and not
+    // dependent on order of the files returned by directory_iterator.
+    std::sort(channels.begin(), channels.end());
+    for (uint8_t i = 0;
+         i < std::min(channelCount, static_cast<uint8_t>(channels.size())); ++i)
+    {
+        output.push_back(walkChannel(basePath, channels[i], cache));
+    }
+
+    return output;
+}
+
+std::vector<uint8_t>
+    BifurcationDynamic::walkI2CTreeBifurcation(std::string_view basePath,
+                                               uint8_t bus, Cache& cache)
+{
+    auto path = fmt::format("{}/i2c-{}", basePath, bus);
+    auto findBus = cache.find(path);
+    if (findBus != cache.end())
+    {
+        return findBus->second;
+    }
+
+    // Set the cache with empty result to prevent loops.
+    cache.emplace(path, std::vector<uint8_t>());
+    auto maybePCIeDevice = pcieResources.find(bus);
+    if (maybePCIeDevice == pcieResources.end())
+    {
+        return {};
+    }
+
+    if (maybePCIeDevice->second.type == PCIe::Type::Device)
+    {
+        return {maybePCIeDevice->second.lanes};
+    }
+
+    std::vector<std::vector<uint8_t>> output;
+
+    for (const auto& entry : std::filesystem::directory_iterator(path))
+    {
+        const std::string& i2cPath = entry.path().filename();
+
+        if (i2cPath.starts_with(fmt::format("{}-", bus)))
+        {
+            auto muxPath = fmt::format("{}/{}", path, i2cPath);
+            output = walkMux(basePath, muxPath,
+                             maybePCIeDevice->second.channels, cache);
+            break;
+        }
+    }
+
+    std::vector<uint8_t> result;
+    uint8_t lanes = maybePCIeDevice->second.lanes;
+    uint8_t channels = maybePCIeDevice->second.channels;
+    uint8_t extraLanes = 0;
+    uint8_t emptyCount = 0;
+    for (const auto& out : output)
+    {
+        if (!out.empty())
+        {
+            for (uint8_t count : out)
+            {
+                if (count > lanes)
+                {
+                    return result;
+                }
+                lanes -= count;
+            }
+        }
+        else
+        {
+            ++emptyCount;
+        }
+    }
+
+    if (emptyCount != 0 && emptyCount < channels)
+    {
+        extraLanes = lanes % emptyCount;
+        lanes = lanes / emptyCount;
+        for (auto& out : output)
+        {
+            if (out.empty())
+            {
+                uint8_t currentLane =
+                    lanes + static_cast<uint8_t>(extraLanes-- != 0);
+                out = std::vector<uint8_t>{currentLane};
+            }
+        }
+    }
+
+    for (const auto& out : output)
+    {
+        result.insert(result.end(), out.begin(), out.end());
+    }
+
+    // Override the cache with real result.
+    cache.emplace(path, result);
+    return result;
+}
+
+} // namespace ipmi
+} // namespace google
--- handler.cpp
+++ handler.cpp
@@ -18,6 +18,7 @@
 #include "util.hpp"
 
 #include <fcntl.h>
+#include <fmt/format.h>
 #include <ipmid/api.h>
 #include <mtd/mtd-abi.h>
 #include <mtd/mtd-user.h>
@@ -29,6 +30,7 @@
 #include <filesystem>
 #include <fstream>
 #include <map>
+#include <memory>
 #include <nlohmann/json.hpp>
 #include <phosphor-logging/elog-errors.hpp>
 #include <phosphor-logging/log.hpp>
@@ -37,6 +39,7 @@
 #include <string>
 #include <string_view>
 #include <tuple>
+#include <unordered_set>
 #include <variant>
 #include <xyz/openbmc_project/Common/error.hpp>
 
@@ -622,10 +625,54 @@ void Handler::accelOobWrite(std::string_view name, uint64_t address,
     }
 }
 
-std::vector<uint8_t> Handler::pcieBifurcation(uint8_t index)
+std::vector<uint8_t> Handler::pcieBifurcation(uint8_t index, bool dynamic)
 {
-    return bifurcationHelper.get().getBifurcation(index).value_or(
-        std::vector<uint8_t>{});
+    if (!dynamic)
+    {
+        return bifurcationHelper.get().getBifurcation(index).value_or(
+            std::vector<uint8_t>{});
+    }
+
+    static const std::vector<Json> empty{};
+    std::string name;
+    std::optional<uint8_t> instanceNum;
+
+    try
+    {
+        // Parse the JSON config file.
+        if (!_entityConfigParsed)
+        {
+            _entityConfig = parseConfig(_configFile);
+            _entityConfigParsed = true;
+        }
+
+        std::vector<Json> readings = _entityConfig.value("add_in_card", empty);
+
+        for (const auto& j : readings)
+        {
+            name = j.value("name", "");
+            auto num = j.value("instance", 0);
+
+            if (name == fmt::format("/PE{}", index))
+            {
+                instanceNum = num;
+                break;
+            }
+        }
+    }
+    catch (InternalFailure& e)
+    {
+        throw IpmiException(::ipmi::ccUnspecifiedError);
+    }
+
+    if (instanceNum == std::nullopt)
+    {
+        return {};
+    };
+
+    return bifurcationHelper.get()
+        .getBifurcation(*instanceNum)
+        .value_or(std::vector<uint8_t>{});
 }
 
 } // namespace ipmi
