909ba954fd80f43e191fe9caa0669b1f9417427d
Willy Tu <wltu@google.com>
2021-10-10 13:34:41 -0700

    pcie_bifurcation: Fetch highest level bifurcation at a PCIe slot
    Use the MaxLanes property defined in PCIeDevice and Lanes property
    defined in PCIeSlot to construct the bifurication info.
    
    The command will parse the i2c tree until it hit the PCIe device and
    return the lanes that it uses. At each PCIe slots, it will try to find
    the devices connected to it. All of the PCIe Devices/Slots needs to be
    avaliable in FruDevice for this to work.
    
    Tested:
    Unit tests passed.
    
    Physical Tests.
    
    PE0 -> no bifurcation
    PE1 -> x8x8
    
    ```
    $ ipmitool raw 0x2e 0x32 0x79 0x2b 0x00 0x0b 0
     79 2b 00 0b 00
    
    $ ipmitool raw 0x2e 0x32 0x79 0x2b 0x00 0x0b 1
     79 2b 00 0b 02 08 08
    ```
    
    Change-Id: Id811897cad82a36da29cd8795f8bc6e855a7e96d
    Signed-off-by: Willy Tu <wltu@google.com>

--- handler_impl.hpp
+++ handler_impl.hpp
@@ -14,10 +14,12 @@
 
 #pragma once
 
+#include "bifurcation.hpp"
 #include "handler.hpp"
 
 #include <cstdint>
 #include <map>
+#include <memory>
 #include <nlohmann/json.hpp>
 #include <sdbusplus/bus.hpp>
 #include <string>
@@ -37,7 +39,12 @@ class Handler : public HandlerInterface
 {
   public:
     explicit Handler(const std::string& entityConfigPath = defaultConfigFile) :
-        _configFile(entityConfigPath){};
+        _configFile(entityConfigPath),
+        bifurcationHelper(Bifurcation::createBifurcation()){};
+    Handler(BifurcationInterface* bifurcationHelper,
+            const std::string& entityConfigPath = defaultConfigFile) :
+        _configFile(entityConfigPath),
+        bifurcationHelper(reinterpret_cast<Bifurcation*>(bifurcationHelper)){};
     ~Handler() = default;
 
     std::tuple<std::uint8_t, std::string>
@@ -54,6 +61,8 @@ class Handler : public HandlerInterface
     void hostPowerOffDelay(std::uint32_t delay) const override;
     std::tuple<std::uint32_t, std::string>
         getI2cEntry(unsigned int entry) const override;
+    std::vector<uint8_t> pcieBifurcation(uint8_t index,
+                                         const std::string& i2cPath) override;
 
     uint32_t accelOobDeviceCount() const override;
     std::string accelOobDeviceName(size_t index) const override;
@@ -89,6 +98,8 @@ class Handler : public HandlerInterface
     nlohmann::json _entityConfig{};
 
     std::vector<std::tuple<uint32_t, std::string>> _pcie_i2c_map;
+
+    Bifurcation* bifurcationHelper;
 };
 
 /**
