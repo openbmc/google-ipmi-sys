381b0e6311e889fbbf2ce7d30cef18217f3c04bf
Willy Tu <wltu@google.com>
2021-10-10 13:34:41 -0700

    pcie_bifurcation: Add IPMI command to fetch the highest level bifurcation at a PCIe slot
    Use the new FruDevice Decreator `Bifurcation` to fetch the bifurcation at each device.
    
    Tested:
    Unit tests passed.
    
    Physical Tests.
    
    PE0 -> no bifurcation
    PE1 -> x8x8
    
    ```
    $ ipmitool raw 0x2e 0x32 0x79 0x2b 0x00 0x0b 0
     79 2b 00 0b 00
    
    $ ipmitool raw 0x2e 0x32 0x79 0x2b 0x00 0x0b 1
     79 2b 00 0b 02 08 08
    ```
    
    Change-Id: Id811897cad82a36da29cd8795f8bc6e855a7e96d
    Signed-off-by: Willy Tu <wltu@google.com>

---
+++ test/pcie_bifurcation_unittest.cpp
@@ -0,0 +1,75 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "commands.hpp"
+#include "handler_mock.hpp"
+#include "helper.hpp"
+#include "pcie_bifurcation.hpp"
+
+#include <vector>
+
+#include <gtest/gtest.h>
+
+using ::testing::Return;
+
+namespace google
+{
+namespace ipmi
+{
+
+using testing::_;
+
+TEST(PcieBifurcationCommandTest, InvalidRequest)
+{
+    std::vector<std::uint8_t> request = {};
+
+    HandlerMock hMock;
+    EXPECT_EQ(::ipmi::responseReqDataLenInvalid(),
+              pcieBifurcation(request, &hMock));
+}
+
+TEST(PcieBifurcationCommandTest, ValidRequest)
+{
+    std::vector<std::uint8_t> request = {5};
+    std::vector<uint8_t> expectedOutput = {4, 8, 4};
+
+    HandlerMock hMock;
+    EXPECT_CALL(hMock, pcieBifurcation(5, _)).WillOnce(Return(expectedOutput));
+
+    auto reply = pcieBifurcation(request, &hMock);
+    auto result = ValidateReply(reply);
+    auto& data = result.second;
+
+    EXPECT_EQ(sizeof(struct PcieBifurcationReply) + expectedOutput.size(),
+              data.size());
+    EXPECT_EQ(SysOEMCommands::SysPCIeSlotBifurcation, result.first);
+    ASSERT_EQ(data[0], expectedOutput.size());
+    EXPECT_EQ(data[1], 8);
+    EXPECT_EQ(data[2], 4);
+    EXPECT_EQ(data[3], 4);
+}
+
+TEST(PcieBifurcationCommandTest, ReplyExceddedMaxValue)
+{
+    std::vector<std::uint8_t> request = {5};
+    std::vector<uint8_t> expectedOutput(64, 1);
+
+    HandlerMock hMock;
+    EXPECT_CALL(hMock, pcieBifurcation(5, _)).WillOnce(Return(expectedOutput));
+    EXPECT_EQ(::ipmi::responseInvalidCommand(),
+              pcieBifurcation(request, &hMock));
+}
+
+} // namespace ipmi
+} // namespace google
