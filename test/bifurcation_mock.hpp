6ac75b36b770a649577613f3146f3a32fe2be644
Willy Tu <wltu@google.com>
2021-10-10 13:34:41 -0700

    pcie_bifurcation: Fetch highest level bifurcation at a PCIe slot
    Use the MaxLanes property defined in PCIeDevice and Lanes property
    defined in PCIeSlot to construct the bifurcation info.
    
    The command will parse the i2c tree until it hit the PCIe device and
    return the lanes that it uses. At each PCIe slots, it will try to find
    the devices connected to it. All of the PCIe Devices/Slots needs to be
    available in FruDevice for this to work.
    
    Tested:
    Unit tests passed.
    
    Physical Tests.
    
    PE0 -> no bifurcation
    PE1 -> x8x8
    
    ```
    $ ipmitool raw 0x2e 0x32 0x79 0x2b 0x00 0x0f 0
     79 2b 00 0b 00
    
    $ ipmitool raw 0x2e 0x32 0x79 0x2b 0x00 0x0f 1
     79 2b 00 0b 02 08 08
    ```
    
    Change-Id: Id811897cad82a36da29cd8795f8bc6e855a7e96d
    Signed-off-by: Willy Tu <wltu@google.com>

---
+++ test/bifurcation_mock.hpp
@@ -0,0 +1,53 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "bifurcation.hpp"
+
+#include <ipmid/message.hpp>
+
+#include <optional>
+#include <string>
+#include <tuple>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+namespace google
+{
+namespace ipmi
+{
+
+class BifurcationDynamicMock : public BifurcationDynamic
+{
+  public:
+    ~BifurcationDynamicMock() = default;
+
+    MOCK_METHOD(std::optional<uint8_t>, i2cBus,
+                (::ipmi::Context::ptr, const std::string&),
+                (noexcept, override));
+    MOCK_METHOD(std::optional<uint64_t>, pcieDeviceMaxLanes,
+                (::ipmi::Context::ptr, const std::string&),
+                (noexcept, override));
+    MOCK_METHOD((std::optional<uint64_t>), pcieSlotLanes,
+                (::ipmi::Context::ptr, const std::string&),
+                (noexcept, override));
+    MOCK_METHOD(std::vector<std::string>, physicalAssociations,
+                (::ipmi::Context::ptr, const std::string&),
+                (noexcept, override));
+};
+
+} // namespace ipmi
+} // namespace google
