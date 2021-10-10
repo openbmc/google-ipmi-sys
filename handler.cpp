cc0169d17c6c443ae5c2b64e5b64c90cb37330eb
Willy Tu <wltu@google.com>
2021-10-10 13:34:41 -0700

    pcie_bifurcation: Fetch highest level bifurcation at a PCIe slot
    Use the MaxLanes property defined in PCIeDevice and Lanes property
    defined in PCIeSlot to construct the bifurication info.
    
    The command will parse the i2c tree until it hit the PCIe device and
    return the lanes that it uses. At each PCIe slots, it will try to find
    the devices connected to it. All of the PCIe Devices/Slots needs to be
    avaliable in FruDevice for this to work.
    
    Tested:
    Unit tests passed.
    
    Physical Tests.
    
    PE0 -> no bifurcation
    PE1 -> x8x8
    
    ```
    $ ipmitool raw 0x2e 0x32 0x79 0x2b 0x00 0x0b 0
     79 2b 00 0b 00
    
    $ ipmitool raw 0x2e 0x32 0x79 0x2b 0x00 0x0b 1
     79 2b 00 0b 02 08 08
    ```
    
    Change-Id: Id811897cad82a36da29cd8795f8bc6e855a7e96d
    Signed-off-by: Willy Tu <wltu@google.com>

--- handler.cpp
+++ handler.cpp
@@ -19,6 +19,7 @@
 #include "util.hpp"
 
 #include <fcntl.h>
+#include <fmt/format.h>
 #include <ipmid/api.h>
 #include <mtd/mtd-abi.h>
 #include <mtd/mtd-user.h>
@@ -30,6 +31,7 @@
 #include <filesystem>
 #include <fstream>
 #include <map>
+#include <memory>
 #include <nlohmann/json.hpp>
 #include <phosphor-logging/elog-errors.hpp>
 #include <phosphor-logging/log.hpp>
@@ -38,6 +40,7 @@
 #include <string>
 #include <string_view>
 #include <tuple>
+#include <unordered_set>
 #include <variant>
 #include <xyz/openbmc_project/Common/error.hpp>
 
@@ -597,6 +600,51 @@ void Handler::accelOobWrite(std::string_view name, uint64_t address,
                         entry("DBUS_ARG_DATA=%016llx", data));
         throw IpmiException(IPMI_CC_UNSPECIFIED_ERROR);
     }
+}
+
+std::vector<uint8_t> Handler::pcieBifurcation(uint8_t index,
+                                              const std::string& path)
+{
+    static const std::vector<Json> empty{};
+    std::string name;
+    std::optional<uint8_t> instanceNum;
+
+    try
+    {
+        // Parse the JSON config file.
+        if (!_entityConfigParsed)
+        {
+            _entityConfig = parseConfig(_configFile);
+            _entityConfigParsed = true;
+        }
+
+        std::vector<Json> readings = _entityConfig.value("add_in_card", empty);
+
+        for (const auto& j : readings)
+        {
+            name = j.value("name", "");
+            auto num = j.value("instance", 0);
+
+            if (name == fmt::format("/PE{}", index))
+            {
+                instanceNum = num;
+                break;
+            }
+        }
+    }
+    catch (InternalFailure& e)
+    {
+        throw IpmiException(::ipmi::ccUnspecifiedError);
+    }
+
+    if (!instanceNum)
+    {
+        return {};
+    }
+
+    Cache cache;
+
+    return bifurcationHelper->walkI2CTreeBifurcation(path, *instanceNum, cache);
 }
 
 } // namespace ipmi
