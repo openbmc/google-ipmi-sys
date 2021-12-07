deab4c86430b5600481ee3420131f9ba82eb2cbe
Michael Shen <gpgpgp@google.com>
2021-12-07 09:21:39 +0800

    add SaveHostBootTime OEM command
    This command allow BIOS/Linuxboot to expose the boot time for each stage
    to BMC.
    Those data will be aggregated into `metrics-ipmi-blobs`.
    
    Tested:
    ```
    root@platform:/# ipmitool raw 0x2e 0x32 0x79 0x2b 0x00 0x0b 0x02 0x01 0x02 0x03 0x04 0x05 0x06 0x07 0x08
     79 2b 00 0b
    root@platform:/# cat /run/host_boot_time.json
    {"2":578437695752307201} # 0x0807060504030201 = 578437695752307201(dec)
    ```
    
    Signed-off-by: Michael Shen <gpgpgp@google.com>
    Change-Id: I7ed3ce2045e3fa7c65860e8689d8e5b1fb0cb918

---
+++ test/linuxboot_boot_time_unittest.cpp
@@ -0,0 +1,75 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "commands.hpp"
+#include "handler_mock.hpp"
+#include "helper.hpp"
+#include "linuxboot_boot_time.hpp"
+
+#include <cstdint>
+#include <cstring>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+namespace google
+{
+namespace ipmi
+{
+
+TEST(LinuxbootBootTimeCommandTest, InvalidRequestLength)
+{
+    std::vector<std::uint8_t> request = {};
+    HandlerMock hMock;
+
+    EXPECT_EQ(::ipmi::responseReqDataLenInvalid(),
+              saveLinuxbootBootTime(request, &hMock));
+}
+
+TEST(LinuxbootBootTimeCommandTest, InvalidComponentCode)
+{
+    constexpr uint8_t kInvalidComponentCode = 0xFF;
+
+    std::span<std::uint8_t> request(
+        sizeof(struct SaveLinuxbootBootTimeRequest));
+    request[0] = kInvalidComponentCode;
+    HandlerMock hMock;
+
+    EXPECT_EQ(::ipmi::responseParmOutOfRange(),
+              saveLinuxbootBootTime(request, &hMock));
+}
+
+TEST(LinuxbootBootTimeCommandTest, ValidRequest)
+{
+    constexpr uint64_t kDuration = 845296639; // ~= 14 minutes
+
+    std::vector<std::uint8_t> request(
+        sizeof(struct SaveLinuxbootBootTimeRequest));
+    struct SaveLinuxbootBootTimeRequest requestContents;
+    requestContents.component =
+        static_cast<uint8_t>(ComponentCode::kLinuxbootKernel);
+    requestContents.duration_us = kDuration;
+    memcpy(request.data(), &requestContents, sizeof(requestContents));
+
+    HandlerMock hMock;
+    EXPECT_CALL(hMock, saveLinuxbootBootTime(requestContents.component,
+                                             requestContents.duration_us));
+
+    auto reply = saveLinuxbootBootTime(request, &hMock);
+    auto result = ValidateReply(reply, false);
+    EXPECT_EQ(SysOEMCommands::SysLinuxbootBootTime, result.first);
+}
+
+} // namespace ipmi
+} // namespace google
