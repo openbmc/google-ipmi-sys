0247259b207be0aad2d9e95c1b259dba4285f02e
Michael Shen <gpgpgp@google.com>
2022-06-17 15:16:47 +0000

    host-boot-time: Add `setDuration` and `notify` command
    These 2 commands are for the `btmanager` feature in the google-misc.
    Basically `btmanager` is a daemon for monitoring the host reboot process.
    This is useful for profiling the reboot process and knowing how to
    optimize it.
    
    Reference: https://gerrit.openbmc.org/c/openbmc/google-misc/+/54398
    Reference: https://gerrit.openbmc.org/c/openbmc/google-misc/+/54399
    Reference: https://gerrit.openbmc.org/c/openbmc/google-misc/+/54420
    Reference: https://gerrit.openbmc.org/c/openbmc/google-misc/+/54421
    Reference: https://gerrit.openbmc.org/c/openbmc/google-misc/+/54422
    Reference: https://gerrit.openbmc.org/c/openbmc/google-misc/+/54705
    Reference: https://gerrit.openbmc.org/c/openbmc/google-misc/+/54501
    
    Tested:
    ```
    machine# cat /usr/share/btmanager/resume.json | grep "ABC"
    machine# ipmitool raw 0x2e 0x32 0x79 0x2b 0x00 0x10 0x03 0x41 0x42 0x43 0xa5 0xbb 0x0d 0x00 0x00 0x00 0x00 0x00
     79 2b 00 10 01
    machine# cat /usr/share/btmanager/resume.json | grep "ABC"
                "ABC": 900005,
    
    machine# cat /usr/share/btmanager/resume.json | grep "CurrentTimePoint"
            "CurrentTimePoint": 3
    machine# ipmitool raw 0x2e 0x32 0x79 0x2b 0x00 0x11 0x04
    [DEBUG]: current timepoint = 3, next timepoint = 4, timestamp = 40959110
     79 2b 00 11 86 fc 70 02 00 00 00 00
    machine# cat /usr/share/btmanager/final.json | grep "CurrentTimePoint"
            "CurrentTimePoint": 4
    ```
    
    Signed-off-by: Michael Shen <gpgpgp@google.com>
    Change-Id: I7ed3ce2045e3fa7c65860e8689d8e5b1fb0cb918

---
+++ host_boot_time.cpp
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2022 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "host_boot_time.hpp"
+
+#include "commands.hpp"
+#include "errors.hpp"
+#include "handler.hpp"
+
+#include <cstdint>
+#include <cstring>
+#include <ipmid/api-types.hpp>
+#include <optional>
+#include <regex>
+#include <span>
+#include <string>
+#include <vector>
+
+namespace google
+{
+namespace ipmi
+{
+
+Resp hostBootTimeSetDuration(std::span<const uint8_t> data,
+                             const HandlerInterface* handler)
+{
+    if (data.size() < sizeof(struct HostBootTimeSetDurationRequestHeaderTailer))
+    {
+        std::fprintf(stderr, "Invalid command length: %u\n",
+                     static_cast<unsigned int>(data.size()));
+        return ::ipmi::responseReqDataLenInvalid();
+    }
+    const uint8_t length = data[0];
+    if (data.size() <
+        sizeof(struct HostBootTimeSetDurationRequestHeaderTailer) + length)
+    {
+        std::fprintf(stderr, "Invalid command length: %u\n",
+                     static_cast<unsigned int>(data.size()));
+        return ::ipmi::responseReqDataLenInvalid();
+    }
+
+    const std::string name(data.data() + 1, data.data() + 1 + length);
+    // name must >= 1 byte also allow [0-9a-zA-Z_] only.
+    if (!std::regex_match(name, std::regex("^[0-9a-zA-Z_]+$")))
+    {
+        std::fprintf(stderr,
+                     "Invalid duration name, only [0-9a-zA-Z_] are allowed");
+        return ::ipmi::responseParmOutOfRange();
+    }
+    uint64_t durationMs = 0;
+    memcpy(&durationMs, data.data() + 1 + length, sizeof(durationMs));
+
+    std::vector<uint8_t> reply(sizeof(struct HostBootTimeSetDurationReply));
+    try
+    {
+        reply[0] = handler->hostBootTimeSetDuration(name, durationMs);
+    }
+    catch (const IpmiException& e)
+    {
+        return ::ipmi::response(e.getIpmiError());
+    }
+
+    return ::ipmi::responseSuccess(SysOEMCommands::SysHostBootTimeSetDuration,
+                                   reply);
+}
+
+Resp hostBootTimeNotify(std::span<const uint8_t> data,
+                        const HandlerInterface* handler)
+{
+    constexpr const int kCheckPointCodeEnd = 5;
+
+    if (data.size() < sizeof(struct HostBootTimeNotifyRequest))
+    {
+        std::fprintf(stderr, "Invalid command length: %u\n",
+                     static_cast<unsigned int>(data.size()));
+        return ::ipmi::responseReqDataLenInvalid();
+    }
+
+    const uint8_t checkPointCode = data[0];
+    if (checkPointCode >= kCheckPointCodeEnd)
+    {
+        std::fprintf(stderr, "Undefined Host boot time checkpoint code: %u\n",
+                     checkPointCode);
+        return ::ipmi::responseParmOutOfRange();
+    }
+
+    uint64_t timestamp_ms;
+    try
+    {
+        timestamp_ms = handler->hostBootTimeNotify(checkPointCode);
+        if (timestamp_ms == 0)
+        {
+            std::fprintf(
+                stderr,
+                "Notify failed, check point(%u) shouldn't be sent now.\n",
+                checkPointCode);
+            return ::ipmi::responseParmOutOfRange();
+        }
+    }
+    catch (const IpmiException& e)
+    {
+        return ::ipmi::response(e.getIpmiError());
+    }
+
+    std::vector<uint8_t> reply(sizeof(struct HostBootTimeNotifyReply));
+    memcpy(reply.data(), &timestamp_ms, sizeof(struct HostBootTimeNotifyReply));
+    return ::ipmi::responseSuccess(SysOEMCommands::SysHostBootTimeNotify,
+                                   reply);
+}
+
+} // namespace ipmi
+} // namespace google
