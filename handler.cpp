09921bdbbaa44d5487baeb2cb64d56c6ca22759f
Brandon Kim <brandonkim@google.com>
2023-03-17 18:49:00 +0000

    handler: Add check for file existence for BM mode
    /tmp/BMReady.flag indicates that we are in BM mode.
    
    Tested:
    Verified both cases -
    - Mar 21 11:17:52 ipmid[1130]: /tmp/BMReady.flag exists so we must be in BM mode
    - Mar 21 10:37:04 ipmid[1076]: Unable to find /tmp/BMReady so we must not be in BM mode
    
    Signed-off-by: Brandon Kim <brandonkim@google.com>
    Change-Id: I9fa99b40afea7e48ce7e1f9c47e8f081b71b897f

--- handler.cpp
+++ handler.cpp
@@ -69,11 +69,19 @@ using InternalFailure =
 
 uint8_t isBmcInBareMetalMode()
 {
-#if BARE_METAL
-    return static_cast<uint8_t>(BmcMode::BM_MODE);
-#else
+    static constexpr auto bmSignalPath = "/tmp/BMReady.flag";
+    std::error_code ec;
+
+    if (fs::exists(bmSignalPath, ec))
+    {
+        std::fprintf(stderr, "%s exists so we must be in BM mode\n",
+                     bmSignalPath);
+        return static_cast<uint8_t>(BmcMode::BM_MODE);
+    }
+
+    std::fprintf(stderr, "Unable to find %s so we must not be in BM mode\n",
+                 bmSignalPath);
     return static_cast<uint8_t>(BmcMode::NON_BM_MODE);
-#endif
 }
 
 uint8_t Handler::getBmcMode()
