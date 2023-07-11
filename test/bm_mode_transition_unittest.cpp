f318eb582819a872eb2989812aa1d28002574494
Hao Zhou <haoooamazing@google.com>
2023-07-11 20:18:04 +0000

    File System: Dependency Injection and Mock
    1. Add an abstract FileSystemInterface for Dependency Injection
    2. Add  FileSystemWrapper for real file system operations, which is
       called in produciton
    3. Add FileSystemMock for unit test mock only
    4. Add bm_mode_transition_unittest.cpp to cover file system based bm
       mode transition
    
    Tested:
     1/14 eth                        OK              0.22s
     2/14 flash                      OK              0.21s
     3/14 machine                    OK              0.12s
     4/14 bmc_mode                   OK              0.05s
     5/14 cable                      OK              0.27s
     6/14 cpld                       OK              0.25s
     7/14 entity                     OK              0.24s
     8/14 google_accel_oob           OK              0.20s
     9/14 pcie                       OK              0.11s
    10/14 poweroff                   OK              0.09s
    11/14 psu                        OK              0.08s
    12/14 pcie_bifurcation           OK              0.07s
    13/14 bm_mode_transition         OK              0.29s
    14/14 handler                    OK              0.44s
    
    Ok:                 14
    Expected Fail:      0
    Fail:               0
    Unexpected Pass:    0
    Skipped:            0
    Timeout:            0
    
    Change-Id: I8d0d9096fbeee7af1946d7e73f548e4a249d3bee
    Signed-off-by: Hao Zhou <haoooamazing@google.com>

---
+++ test/bm_mode_transition_unittest.cpp
@@ -0,0 +1,123 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include "bm_config.h"
+
+#include "bmc_mode_enum.hpp"
+#include "errors.hpp"
+#include "file_system_mock.hpp"
+#include "handler.hpp"
+#include "handler_impl.hpp"
+
+#include <charconv>
+#include <filesystem>
+#include <fstream>
+#include <functional>
+#include <memory>
+#include <string>
+#include <tuple>
+
+#include <gtest/gtest.h>
+
+namespace google
+{
+namespace ipmi
+{
+using ::testing::_;
+using ::testing::AnyNumber;
+using ::testing::DoAll;
+using ::testing::Eq;
+using ::testing::IsNull;
+using ::testing::NotNull;
+using ::testing::Pointee;
+using ::testing::Return;
+using ::testing::ReturnNull;
+using ::testing::StrEq;
+
+namespace fs = std::filesystem;
+
+static constexpr auto bmDriveCleaningFlagPath = "/run/bm-drive-cleaning.flag";
+static constexpr auto bmDriveCleaningDoneFlagPath =
+    "/run/bm-drive-cleaning-done.flag";
+static constexpr auto bmDriveCleaningDoneAckFlagPath =
+    "/run/bm-drive-cleaning-done-ack.flag";
+// static constexpr auto BM_SIGNAL_PATH = "/run/bm-ready.flag";
+
+class MockFsHandler : public Handler
+{
+  public:
+    MockFsHandler(std::unique_ptr<FileSystemMock> mock,
+                  const std::string& config = "") :
+        Handler(config)
+    {
+        fsPtr = move(mock);
+    }
+};
+
+TEST(BmcModeTransitionTest, DriveCleaningDoneAckFlag)
+{
+    auto fsMockPtr = std::make_unique<FileSystemMock>();
+    EXPECT_CALL(*fsMockPtr, exists(fs::path(bmDriveCleaningDoneAckFlagPath), _))
+        .WillOnce(Return(true));
+
+    MockFsHandler h(fsMockPtr);
+    EXPECT_EQ(h.getBmcMode(), static_cast<uint8_t>(BmcMode::BM_MODE));
+}
+
+TEST(BmcModeTransitionTest, DriveCleaningDoneFlag)
+{
+    auto fsMockPtr = std::make_unique<FileSystemMock>();
+    EXPECT_CALL(*fsMockPtr, exists(fs::path(bmDriveCleaningDoneAckFlagPath), _))
+        .WillOnce(Return(false));
+    EXPECT_CALL(*fsMockPtr, exists(fs::path(bmDriveCleaningDoneFlagPath), _))
+        .WillOnce(Return(true));
+    EXPECT_CALL(*fsMockPtr, rename(fs::path(bmDriveCleaningDoneFlagPath),
+                                   fs::path(bmDriveCleaningDoneAckFlagPath), _))
+        .Times(1);
+
+    MockFsHandler h(fsMockPtr);
+    EXPECT_EQ(h.getBmcMode(), static_cast<uint8_t>(BmcMode::BM_MODE));
+}
+
+TEST(BmcModeTransitionTest, FirstCleaningFlag)
+{
+    auto fsMockPtr = std::make_unique<FileSystemMock>();
+    EXPECT_CALL(*fsMockPtr, exists(fs::path(bmDriveCleaningDoneAckFlagPath), _))
+        .WillOnce(Return(false));
+    EXPECT_CALL(*fsMockPtr, exists(fs::path(bmDriveCleaningDoneFlagPath), _))
+        .WillOnce(Return(false));
+    EXPECT_CALL(*fsMockPtr, exists(fs::path(BM_SIGNAL_PATH), _))
+        .WillOnce(Return(true));
+    EXPECT_CALL(*fsMockPtr, exists(fs::path(bmDriveCleaningFlagPath), _))
+        .WillOnce(Return(false));
+    EXPECT_CALL(*fsMockPtr, create(StrEq(bmDriveCleaningFlagPath))).Times(1);
+
+    MockFsHandler h(fsMockPtr);
+    EXPECT_EQ(h.getBmcMode(), static_cast<uint8_t>(BmcMode::BM_CLEANING_MODE));
+}
+
+TEST(BmcModeTransitionTest, NonBmMode)
+{
+    auto fsMockPtr = std::make_unique<FileSystemMock>();
+    EXPECT_CALL(*fsMockPtr, exists(fs::path(bmDriveCleaningDoneAckFlagPath), _))
+        .WillOnce(Return(false));
+    EXPECT_CALL(*fsMockPtr, exists(fs::path(bmDriveCleaningDoneFlagPath), _))
+        .WillOnce(Return(false));
+    EXPECT_CALL(*fsMockPtr, exists(fs::path(BM_SIGNAL_PATH), _))
+        .WillOnce(Return(false));
+    MockFsHandler h(fsMockPtr);
+    EXPECT_EQ(h.getBmcMode(), static_cast<uint8_t>(BmcMode::NON_BM_MODE));
+}
+
+} // namespace ipmi
+} // namespace google
