fceaa563c348df57356dc9184383367e87e16474
Hao Zhou <haoooamazing@google.com>
2023-07-11 20:18:04 +0000

    File System: Dependency Injection and Mock
    1. Add an abstract FileSystemInterface for Dependency Injection
    2. Add  FileSystemWrapper for real file system operations, which is
       called in produciton
    3. Add FileSystemMock for unit test mock only
    4. Add bm_mode_transition_unittest.cpp to cover file system based bm
       mode transition
    
    Tested:
     1/14 eth                        OK              0.22s
     2/14 flash                      OK              0.21s
     3/14 machine                    OK              0.12s
     4/14 bmc_mode                   OK              0.05s
     5/14 cable                      OK              0.27s
     6/14 cpld                       OK              0.25s
     7/14 entity                     OK              0.24s
     8/14 google_accel_oob           OK              0.20s
     9/14 pcie                       OK              0.11s
    10/14 poweroff                   OK              0.09s
    11/14 psu                        OK              0.08s
    12/14 pcie_bifurcation           OK              0.07s
    13/14 bm_mode_transition         OK              0.29s
    14/14 handler                    OK              0.44s
    
    Ok:                 14
    Expected Fail:      0
    Fail:               0
    Unexpected Pass:    0
    Skipped:            0
    Timeout:            0
    
    Change-Id: I8d0d9096fbeee7af1946d7e73f548e4a249d3bee
    Signed-off-by: Hao Zhou <haoooamazing@google.com>

--- handler_impl.hpp
+++ handler_impl.hpp
@@ -1,4 +1,4 @@
-// Copyright 2022 Google LLC
+// Copyright 2023 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
 #pragma once
 
 #include "bifurcation.hpp"
+#include "file_system_wrapper_impl.hpp"
 #include "handler.hpp"
 
 #include <nlohmann/json.hpp>
@@ -22,6 +23,7 @@
 
 #include <cstdint>
 #include <map>
+#include <memory>
 #include <string>
 #include <string_view>
 #include <tuple>
@@ -39,12 +41,13 @@ class Handler : public HandlerInterface
 {
   public:
     explicit Handler(const std::string& entityConfigPath = defaultConfigFile) :
+        fsPtr(std::make_shared<FileSystemWrapper>()),
         _configFile(entityConfigPath),
         bifurcationHelper(BifurcationStatic::createBifurcation()){};
     Handler(std::reference_wrapper<BifurcationInterface> bifurcationHelper,
             const std::string& entityConfigPath = defaultConfigFile) :
-        _configFile(entityConfigPath),
-        bifurcationHelper(bifurcationHelper){};
+        fsPtr(std::make_shared<FileSystemWrapper>()),
+        _configFile(entityConfigPath), bifurcationHelper(bifurcationHelper){};
     ~Handler() = default;
 
     uint8_t getBmcMode() override;
@@ -71,12 +74,16 @@ class Handler : public HandlerInterface
     void accelOobWrite(std::string_view name, uint64_t address,
                        uint8_t num_bytes, uint64_t data) const override;
     void linuxBootDone() const override;
+    std::shared_ptr<FileSystemInterface> fsPtr;
 
   protected:
     // Exposed for dependency injection
     virtual sdbusplus::bus_t getDbus() const;
+    virtual const std::shared_ptr<FileSystemInterface>& getFs() const;
 
   private:
+    // std::shared_ptr<FileSystemInterface> fsPtr;
+
     std::string _configFile;
 
     bool _entityConfigParsed = false;
