54b068b8ebb89aada43d163d4f3161af549269b7
Willy Tu <wltu@google.com>
2024-04-18 21:51:24 +0000

    Fix input to gbmc-enqueue-powercycle.sh
    The input should have PSU_HARDRESET_DELAY prefix when writing to
    `/run/psu_timedelay`. Add the check to make sure we support both type
    of input in case some clients are passing the PSU_HARDRESET_DELAY
    explicitly
    
    Tested:
    ```
    $ cat /tmp/test.sh
    #!/bin/sh
    #
    
    delay="${1}"
    if ! echo "${delay}" | grep -q 'PSU_HARDRESET_DELAY=*' ;
    then
      delay="PSU_HARDRESET_DELAY=${delay}"
    fi
    
    echo "${delay}"
    
    $  /tmp/test.sh 12
    PSU_HARDRESET_DELAY=12
    
    $  /tmp/test.sh PSU_HARDRESET_DELAY=123
    PSU_HARDRESET_DELAY=123
    ```
    
    Change-Id: If7064edd53b95d4e49104bf5de81c8462fffd2d3
    Signed-off-by: Willy Tu <wltu@google.com>

--- gbmc-enqueue-powercycle.sh
+++ gbmc-enqueue-powercycle.sh
@@ -1,4 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "${1}" > "/run/psu_timedelay"
+delay="${1}"
+if [[ "${delay}" !- 'PSU_HARDRESET_DELAY=*' ]] ;
+then
+    delay="PSU_HARDRESET_DELAY=${delay}"
+fi
+
+echo "${delay}" > "/run/psu_timedelay"
 systemctl start gbmc-psu-hardreset.target --no-block
