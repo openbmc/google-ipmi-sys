b8088defdaa3e361ca508b53073ca69d9dd078da
Brandon Kim <brandonkim@google.com>
2024-08-17 00:36:55 +0000

    bios_setting: Add a read handler
    Tested:
    ```
    ~# echo -n "01234567" > /run/oem_bios_setting
    ~# ipmitool raw 0x2e 0x32 0x79 0x2b 0x00 0x18
     79 2b 00 18 08 30 31 32 33 34 35 36 37
    ~# rm /run/oem_bios_setting
    ~# ipmitool raw 0x2e 0x32 0x79 0x2b 0x00 0x18
    Unable to send RAW command (channel=0x0 netfn=0x2e lun=0x0 cmd=0x32 rsp=0xca): Cannot return number of requested data bytes
    ```
    
    Signed-off-by: Brandon Kim <brandonkim@google.com>
    Change-Id: Icde2de4799a751634f56a580351bf10254dd7e4f

---
+++ test/bios_setting_unittest.cpp
@@ -0,0 +1,87 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "bios_setting.hpp"
+#include "commands.hpp"
+#include "helper.hpp"
+
+#include <stdplus/fd/create.hpp>
+#include <stdplus/fd/managed.hpp>
+#include <stdplus/fd/ops.hpp>
+#include <stdplus/gtest/tmp.hpp>
+
+#include <string>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+namespace google
+{
+namespace ipmi
+{
+
+using testing::_;
+using ::testing::ElementsAre;
+
+class BiosSettingTest : public stdplus::gtest::TestWithTmp
+{
+  public:
+    std::string filename = std::format("{}/oem_bios_setting", CaseTmpDir());
+};
+
+TEST_F(BiosSettingTest, NoOrEmptyFileRead)
+{
+    std::vector<uint8_t> request = {};
+
+    HandlerMock hMock;
+    EXPECT_EQ(::ipmi::responseRetBytesUnavailable(),
+              readBiosSetting(request, &hMock));
+    {
+        stdplus::ManagedFd managedFd = stdplus::fd::open(
+            filename, stdplus::fd::OpenFlags(stdplus::fd::OpenAccess::WriteOnly)
+                          .set(stdplus::fd::OpenFlag::Trunc)
+                          .set(stdplus::fd::OpenFlag::Create));
+    }
+    EXPECT_EQ(::ipmi::responseRetBytesUnavailable(),
+              readBiosSetting(request, &hMock, filename));
+    std::remove(filename.c_str());
+}
+
+TEST_F(BiosSettingTest, SuccessfulRead)
+{
+    std::vector<uint8_t> request = {};
+    // Ensure 0x0A which is a new line character '\n', is read properly
+    std::vector<uint8_t> payload = {0x0A, 0xDE, 0xAD, 0xBE, 0xEF, 0x0A};
+    std::vector<uint8_t> expectedReply = {6,    0x0A, 0xDE, 0xAD,
+                                          0xBE, 0xEF, 0x0A};
+
+    stdplus::ManagedFd managedFd = stdplus::fd::open(
+        filename, stdplus::fd::OpenFlags(stdplus::fd::OpenAccess::WriteOnly)
+                      .set(stdplus::fd::OpenFlag::Trunc)
+                      .set(stdplus::fd::OpenFlag::Create));
+    stdplus::fd::writeExact(managedFd, payload);
+
+    HandlerMock hMock;
+    auto reply = readBiosSetting(request, &hMock, filename);
+    auto result = ValidateReply(reply);
+    auto& data = result.second;
+
+    EXPECT_EQ(SysOEMCommands::SysReadBiosSetting, result.first);
+    EXPECT_EQ(expectedReply.size() - 1, data.front());
+    EXPECT_EQ(expectedReply, data);
+    std::remove(filename.c_str());
+}
+
+} // namespace ipmi
+} // namespace google
