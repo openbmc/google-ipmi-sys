4fd7a58fa5ee4d01023021648ade7b4d4f81f07f
Gaurav Gandhi <gauravgandhi@google.com>
2025-07-21 09:46:41 -0700

    Add support for setting VoutSetpoint and NominalTargetVout
    Change-Id: I21e0064bcd8c7ba0352954b639556cc2f8af870f
    Signed-off-by: Gaurav Gandhi <gauravgandhi@google.com>

--- handler_impl.hpp
+++ handler_impl.hpp
@@ -108,10 +108,9 @@ class Handler : public HandlerInterface
         {0x20, "memory_device"}};
 
     const std::unordered_map<uint8_t, std::string> _vrSettingsMap{
-        {0, "idle_mode_"},
-        {1, "power_brake_"},
-        {2, "loadline_"},
-        {3, "vout_margin_"}};
+        {0, "idle_mode_"},     {1, "power_brake_"},
+        {2, "loadline_"},      {3, "vout_margin_"},
+        {4, "vout_setpoint_"}, {5, "nominal_target_vout_"}};
 
     const std::unordered_map<uint8_t, std::string> bmInstanceTypeStringMap = {
         {0x00, "asset-tag"}, {0x01, "board-serial-number"},
