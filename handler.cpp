33d920bf32d152838717cb4122b435a5182c7897
Vikram Gara <vikramgara@google.com>
2026-01-12 05:29:10 +0000

    Added IPMI handler to support core count request
    This change introduces a new OEM IPMI command (SysGetCoreCount) to allow
    the BIOS or host to query the number of CPU cores as reported by the BMC.
    
    The core count is read from /run/cpu_config.json on the BMC. If the file
    is not found or invalid, the command will return an error or 0 cores.
    
    This is needed for the BIOS to dynamically adjust settings based on the
    CPU configuration.
    
    Tested:
    Unit tests added to verify file handling and JSON parsing.
    (MOCKING NOTE: Manual test on target system recommended to confirm
    /run/cpu_config.json path and content)
    
    Change-Id: I946d5ac918a2dae8a94e1b348446075e72007e3e
    Signed-off-by: Vikram Gara <vikramgara+bmc@google.com>

--- handler.cpp
+++ handler.cpp
@@ -15,6 +15,7 @@
 #include "handler.hpp"
 
 #include "bm_config.h"
+#include "config.h"
 
 #include "bm_instance.hpp"
 #include "bmc_mode_enum.hpp"
@@ -807,7 +808,55 @@ std::string Handler::getBMInstanceProperty(uint8_t propertyType) const
         stdplus::print(stderr, "Failed to read: '{}'.\n", opath);
         throw IpmiException(::ipmi::ccUnspecifiedError);
     }
+
     return property;
+}
+
+int Handler::getCoreCount() const
+{
+    const char* path = CPU_CONFIG_PATH;
+    std::error_code ec;
+    if (!this->getFs()->exists(path, ec))
+    {
+        log<level::INFO>("CPU config file not found, returning 0 cores",
+                         entry("PATH=%s", path));
+        return 0;
+    }
+
+    std::ifstream ifs(path);
+    if (!ifs.is_open())
+    {
+        log<level::ERR>("Failed to open CPU config file",
+                        entry("PATH=%s", path));
+        throw IpmiException(::ipmi::ccUnspecifiedError);
+    }
+
+    try
+    {
+        Json data = Json::parse(ifs);
+        if (data.contains("cpu_core_count") && data["cpu_core_count"].is_number_integer())
+        {
+            return data["cpu_core_count"].get<int>();
+        }
+        else
+        {
+            log<level::ERR>("Invalid format in CPU config file",
+                            entry("PATH=%s", path));
+            throw IpmiException(::ipmi::ccUnspecifiedError);
+        }
+    }
+    catch (Json::parse_error& e)
+    {
+        log<level::ERR>("Failed to parse CPU config file",
+                        entry("PATH=%s", path), entry("WHAT=%s", e.what()));
+        throw IpmiException(::ipmi::ccUnspecifiedError);
+    }
+    catch (...)
+    {
+        log<level::ERR>("Unknown error reading CPU config file",
+                        entry("PATH=%s", path));
+        throw IpmiException(::ipmi::ccUnspecifiedError);
+    }
 }
 
 } // namespace ipmi
